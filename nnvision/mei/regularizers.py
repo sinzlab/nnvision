281b3487294e361e1ccfc4ca409c9c1b24c135fd
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-04-14 23:00:21 +0200

    Update MEI regularizers.

--- nnvision/mei/regularizers.py
+++ nnvision/mei/regularizers.py
@@ -1,7 +1,4 @@
-from . import ops
-from . import utils
-
-from .ops import ChangeStd, GaussianBlur
+from featurevis.ops import ChangeStd, GaussianBlur
 
 postup_contrast_01 = ChangeStd(0.1)
 postup_contrast_1 = ChangeStd(1)
