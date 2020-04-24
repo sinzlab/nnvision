88c3456841821ea37da825f7d23289d07818e98a
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-04-24 12:20:15 +0200

    Add regularizers for unnormalized images.

--- nnvision/mei/regularizers.py
+++ nnvision/mei/regularizers.py
@@ -2,4 +2,8 @@ from featurevis.ops import ChangeStd, GaussianBlur
 
 postup_contrast_01 = ChangeStd(0.1)
 postup_contrast_1 = ChangeStd(1)
+postup_contrast_125 = ChangeStd(12.5)
+postup_contrast_100 = ChangeStd(10)
+postup_contrast_5 = ChangeStd(5)
+
 Blur_sigma_1 = GaussianBlur(1)
