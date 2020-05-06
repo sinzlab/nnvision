8f04b489caca2646414fb26a0760ab8a2625b178
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-05-06 15:30:05 +0200

    Add DiCarlos MEI regularizers to regularizer-module.

--- nnvision/mei/regularizers.py
+++ nnvision/mei/regularizers.py
@@ -1,4 +1,6 @@
-from featurevis.ops import ChangeStd, GaussianBlur
+from featurevis.ops import ChangeStd, GaussianBlur, Jitter, TotalVariation, ChangeNorm, ClipRange
+from featurevis.utils import Compose
+
 from .utility import cumstom_initial_guess
 from functools import partial
 
@@ -12,6 +14,12 @@ postup_contrast_5 = ChangeStd(5)
 
 # Blurring
 Blur_sigma_1 = GaussianBlur(1)
+
+
+# Regularizers of DiCarlo 2019
+jitter_DiCarlo = Jitter(max_jitter=(2, 4))
+total_variation_DiCarlo = TotalVariation(weight=0.001)
+gradient_DiCarlo = Compose([ChangeNorm(1), ClipRange(-1, 1)])
 
 # Initial Guess
 rgb_initial_guess = partial(cumstom_initial_guess, mean=111, std=60)
