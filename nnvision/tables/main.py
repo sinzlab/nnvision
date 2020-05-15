4472819ae9150ba0a271ac3020f256bb8c01eed9
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-05-15 22:13:17 +0200

    Refactor MonkeyExperiment into Recoring. which accepts both SUA and MUA.

--- nnvision/tables/main.py
+++ nnvision/tables/main.py
@@ -4,13 +4,13 @@ import datajoint as dj
 from ..utility.measures import get_oracles, get_explainable_var
 from nnfabrik.main import Dataset
 from nnfabrik.utility.dj_helpers import CustomSchema
-
+from ..datasets.conventions import unit_type_conventions
 from featurevis import integration
 
 schema = CustomSchema(dj.config.get('schema_name', 'nnfabrik_core'))
 
 @schema
-class MultiExperiment(dj.Computed):
+class Recording(dj.Computed):
     definition = """
     # This table stores something which is awesome by design
 
@@ -40,16 +40,13 @@ class MultiExperiment(dj.Computed):
         # All Units
 
         -> MultiExperiment.Sessions
-        unit_id:     int
+        unit_id:            int     
+        unit_type:          int
         ---
-        unit_index:  int
-        electrode:   int
-        relative_depth:  float
+        unit_index:         int
+        electrode:          int
+        relative_depth:     float
         """
-
-        def get_output_selected_model(self, model, key):
-            unit_index, data_key = (self & key).fetch1("unit_index", "data_key")
-            return integration.get_output_selected_model(unit_index, data_key, model)
 
     def make(self, key):
 
@@ -72,17 +69,22 @@ class MultiExperiment(dj.Computed):
                     mua_data = pickle.load(mua_pkl)
 
                 if str(mua_data["session_id"]) == data_key:
-                    unit_ids_mua = mua_data["unit_ids"] + 100
+                    unit_ids_mua = mua_data["unit_ids"]
                     electrode_mua = mua_data["electrode_nums"]
                     relative_depth_mua = mua_data["relative_micron_depth"]
+                    unit_types = mua_data["unit_type"]
+
+                    unit_type_mua = np.ones(unit_types.shape[0])
+                    for i, unit in enumerate(unit_types):
+                        unit_type_mua[i] = unit_type_conventions[unit]
                     break
 
             if not str(mua_data["session_id"]) == data_key:
                 print("session {} does not exist in MUA. Skipping MUA for that session".format(data_key))
                 unit_ids_mua, electrode_mua, relative_depth_mua = [],[],[]
 
-            unit_ids = raw_data["unit_ids"] if "unit_ids" in raw_data else np.arange(
-                raw_data["testing_responses"].shape[1])
+            unit_ids = raw_data.get("unit_ids", np.arange(raw_data["testing_responses"].shape[1]))
+            unit_type = raw_data.get("unit_type", np.ones(raw_data["testing_responses"].shape[1]))
 
             electrode = raw_data["electrode_nums"] if "electrode_nums" in raw_data else np.zeros_like(unit_ids,
                                                                                                       dtype=np.double)
@@ -92,9 +94,10 @@ class MultiExperiment(dj.Computed):
                 "relative_micron_depth"] if "relative_micron_depth" in raw_data else np.zeros_like(unit_ids,
                                                                                                    dtype=np.double)
 
-            unit_ids = np.concatenate([unit_ids, unit_ids_mua]).astype('int')
-            electrode = np.concatenate([electrode, electrode_mua]).astype('int')
-            relative_depth = np.concatenate([relative_depth, relative_depth_mua]).astype('int')
+            unit_ids = np.concatenate([unit_ids, unit_ids_mua])
+            unit_type = np.concatenate([unit_type, unit_type_mua])
+            electrode = np.concatenate([electrode, electrode_mua])
+            relative_depth = np.concatenate([relative_depth, relative_depth_mua])
 
             session_dict[data_key] = dict(animal_id=raw_data["subject_id"],
                                           n_neurons=int(len(unit_ids)),
@@ -102,6 +105,7 @@ class MultiExperiment(dj.Computed):
                                           y_grid=y_grid)
 
             session_dict[data_key]['unit_id'] = unit_ids
+            session_dict[data_key]['unit_type'] = unit_type
             session_dict[data_key]['electrode'] = electrode
             session_dict[data_key]['x_grid'] = x_grid
             session_dict[data_key]['y_grid'] = y_grid
@@ -128,6 +132,7 @@ class MultiExperiment(dj.Computed):
             for i, neuron_id in enumerate(session_dict[k]["unit_id"]):
                 key["unit_id"] = int(neuron_id)
                 key["unit_index"] = i
+                key['unit_type'] = session_dict[k]['unit_type'][i]
                 key['electrode'] = session_dict[k]['electrode'][i]
                 key['relative_depth'] = session_dict[k]['relative_depth'][i]
                 self.Units().insert1(key, ignore_extra_fields=True)
