c54bbce487356a7ae4135c6446a7eb256b296113
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-05-19 19:21:57 +0200

    generalize repeat selector function.

--- nnvision/utility/measure_helpers.py
+++ nnvision/utility/measure_helpers.py
@@ -16,8 +16,6 @@ def get_subset_of_repeats(outputs, repeat_limit, randomize=True):
     limited_output = []
     for repetitions in outputs:
         n_repeats = repetitions.shape[0]
-        if repeat_limit > n_repeats:
-            warnings.warn(f"Repeat limit of {repeat_limit}is larger than the repeats of {n_repeats}")
         limited_output.append(repetitions[:repeat_limit, ] if not randomize else repetitions[
-            np.random.choice(n_repeats, repeat_limit, replace=False)])
+            np.random.choice(n_repeats, repeat_limit if repeat_limit < n_repeats else n_repeats, replace=False)])
     return limited_output
