7f99b91c731e33c8fcbf078836b010823b9dc41c
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-06-19 19:40:03 +0200

    Move ensemble scores out of the scoring module.

--- nnvision/tables/scores.py
+++ nnvision/tables/scores.py
@@ -1,16 +1,9 @@
 import datajoint as dj
-from nnfabrik.template import TrainedModelBase
-import tempfile
-import torch
-import os
 from nnfabrik.main import Model, Dataset, Trainer, Seed, Fabrikant
-from nnfabrik.utility.dj_helpers import gitlog, make_hash
-import numpy as np
 from .main import Recording
 from ..utility.measures import get_oracles, get_repeats, get_FEV, get_explainable_var, get_correlations, get_poisson_loss, get_avg_correlations, get_predictions, get_targets
 from .from_nnfabrik import TrainedModel, TrainedTransferModel
-from .from_mei import Ensemble
-from .utility import DataCache, TrainedModelCache, EnsembleModelCache
+from .utility import DataCache, TrainedModelCache
 from nnfabrik.utility.dj_helpers import CustomSchema
 from nnfabrik.template import ScoringBase, SummaryScoringBase
 from .from_nnfabrik import ScoringBaseNeuronType
@@ -115,45 +108,6 @@ class TransferTestCorrelationScore(ScoringBaseNeuronType):
     measure_attribute = "test_correlation"
     data_cache = None
     model_cache = None
-
-
-# ============================= ENSEMBLE SCORES =============================
-
-
-@schema
-class TrainCorrelationEnsembleScore(ScoringBaseNeuronType):
-    trainedmodel_table = Ensemble
-    dataset_table = Dataset
-    unit_table = Recording.Units
-    measure_function = staticmethod(get_correlations)
-    measure_dataset = "train"
-    measure_attribute = "train_correlation"
-    data_cache = DataCache
-    model_cache = EnsembleModelCache
-
-
-@schema
-class ValidationCorrelationEnsembleScore(ScoringBaseNeuronType):
-    trainedmodel_table = Ensemble
-    dataset_table = Dataset
-    unit_table = Recording.Units
-    measure_function = staticmethod(get_correlations)
-    measure_dataset = "validation"
-    measure_attribute = "validation_correlation"
-    data_cache = DataCache
-    model_cache = EnsembleModelCache
-
-
-@schema
-class TestCorrelationEnsembleScore(ScoringBaseNeuronType):
-    trainedmodel_table = Ensemble
-    dataset_table = Dataset
-    unit_table = Recording.Units
-    measure_function = staticmethod(get_correlations)
-    measure_dataset = "test"
-    measure_attribute = "test_correlation"
-    data_cache = DataCache
-    model_cache = EnsembleModelCache
 
 
 # ============================= CUSTOM SCORES =============================
