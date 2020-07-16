b731965f6851d8b8838ea714d2a3eb227b124e09
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-07-16 21:38:52 +0200

    Modify target function to include the regularizing term.

--- nnvision/mei/targets.py
+++ nnvision/mei/targets.py
@@ -2,5 +2,6 @@ import torch
 from functools import partial
 
 
-def sum_over_max(output, dim=0):
-    return torch.max(output, dim=dim).values.sum()
+def mean_of_max(output, max_dim=0, mean_dim=1, gamma=1):
+    return torch.max(output, dim=max_dim).values.mean() + gamma * torch.mean(output, dim=mean_dim).min()
+a
