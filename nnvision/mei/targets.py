0a646573b90ed67959932996aafba786993aeb99
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-07-17 18:41:09 +0200

    get rid of typo.

--- nnvision/mei/targets.py
+++ nnvision/mei/targets.py
@@ -4,4 +4,3 @@ from functools import partial
 
 def mean_of_max(output, max_dim=0, mean_dim=1, gamma=1):
     return torch.max(output, dim=max_dim).values.mean() + gamma * torch.mean(output, dim=mean_dim).min()
-a
