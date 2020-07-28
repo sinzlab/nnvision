7d63b3301808d41e333152eb276e6414650ea5e2
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-07-28 11:12:37 +0200

    Add normalizing responses to MEI target function.

--- nnvision/mei/targets.py
+++ nnvision/mei/targets.py
@@ -2,5 +2,7 @@ import torch
 from functools import partial
 
 
-def mean_of_max(output, max_dim=0, mean_dim=1, gamma=1):
+def mean_of_max(output, max_dim=0, mean_dim=1, gamma=1, normalizing_responses=None):
+    if normalizing_responses is not None:
+        output = output / torch.tensor(normalizing_responses).cuda()
     return torch.max(output, dim=max_dim).values.mean() + gamma * torch.mean(output, dim=mean_dim).min()
