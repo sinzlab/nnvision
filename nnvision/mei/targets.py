a11e901c6e729b180c38fd159fc29b61ad62298d
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-08-06 14:45:36 +0200

    Change initialization of tensors in mei.targets.

--- nnvision/mei/targets.py
+++ nnvision/mei/targets.py
@@ -1,8 +1,8 @@
 import torch
-from functools import partial
+import numpy as np
 
 
 def mean_of_max(output, max_dim=0, mean_dim=1, gamma=1, normalizing_responses=None):
     if normalizing_responses is not None:
-        output = output / torch.tensor(normalizing_responses).cuda()
+        output = output / torch.from_numpy(normalizing_responses).cuda()
     return torch.max(output, dim=max_dim).values.mean() + gamma * torch.mean(output, dim=mean_dim).min()
