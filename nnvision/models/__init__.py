70b4818d915eef37b7f26c1480c5d47d20287497
cgruner <50152424+ClaudiusGruner@users.noreply.github.com>
2020-09-21 16:27:07 +0200

    Update import behavior

--- nnvision/models/__init__.py
+++ nnvision/models/__init__.py
@@ -1,3 +1,3 @@
 from .models import (se_core_point_readout, se_core_full_gauss_readout, se_core_gauss_readout,
                      se_core_spatialXfeature_readout, vgg_core_gauss_readout, stacked2d_core_gaussian_readout, simple_core_transfer,
-                     se_core_shared_gaussian_readout, augmented_full_readout)
+                     se_core_shared_gaussian_readout, augmented_full_readout, stacked2d_core_dn_linear_readout)
