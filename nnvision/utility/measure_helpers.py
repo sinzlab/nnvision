51c0d12f04de09014db5de2ada019f8b2c27c0dd
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-10-29 19:31:30 +0100

    Add cosine mask to be used by ChangeNorm and Mask ops of the mei module.

--- nnvision/utility/measure_helpers.py
+++ nnvision/utility/measure_helpers.py
@@ -1,6 +1,7 @@
 import types
 import warnings
 import numpy as np
+import math
 
 
 def get_subset_of_repeats(outputs, repeat_limit, randomize=True):
@@ -25,4 +26,34 @@ def get_subset_of_repeats(outputs, repeat_limit, randomize=True):
 def is_ensemble_function(model):
     return (isinstance(model, types.FunctionType))
 
+
+def get_cosine_mask(width, height, pixelsPerDegree, fadeStartDegrees):
+
+    fadeStartPixels = round(fadeStartDegrees * pixelsPerDegree)
+    widthBegin = -(width / 2)
+    widthEnd = (width / 2)
+    heightBegin = -(width / 2)
+    heightEnd = (width / 2)
+
+    # Make grid of values for entire extent of image
+    x, y = np.meshgrid(np.arange(widthBegin, widthEnd), np.arange(heightBegin, heightEnd))
+    thresholdRadius = fadeStartPixels / 2
+    # Take norm of grid because disk will be circular
+    normXY = np.sqrt(np.square(x) + np.square(y))
+    remainingRadius = (width - fadeStartPixels) / 2
+    disk = np.zeros((width, height))
+    for i in range(len(normXY)):
+        for j in range(len(normXY)):
+            value = int(int(normXY[i, j] >= thresholdRadius) & int(normXY[i, j] <= width / 2))
+            disk[i, j] = int(value)
+    alteredDisk = disk * (normXY - thresholdRadius) / remainingRadius
+    fill = np.zeros((width, height))
+    for i in range(len(normXY)):
+        for j in range(len(normXY)):
+            value = int(normXY[i, j] > width / 2)
+            fill[i, j] = int(value)
+    finalDisk = (math.pi) / 2 * (alteredDisk + fill)
+    blendingMask = np.cos(finalDisk)
+
+    return blendingMask
 
