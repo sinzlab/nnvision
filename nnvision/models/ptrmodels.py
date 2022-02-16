e19cce2942289cc4853cde84de9f8987b9ed6de0
KonstantinWilleke <konstantin.willeke@gmail.com>
2022-02-16 18:41:44 +0100

    Enable ptrmodels to use stacking arguments in the multihead attention readout.

--- nnvision/models/ptrmodels.py
+++ nnvision/models/ptrmodels.py
@@ -18,7 +18,9 @@ from .readouts import (
     MultipleRemappedGaussian2d,
     MultipleSelfAttention2d,
     MultipleMultiHeadAttention2d,
+    MultipleSharedMultiHeadAttention2d,
 )
+
 from .utility import unpack_data_info
 from .encoders import EncoderShifter, Encoder
 from .shifters import MLPShifter, StaticAffine2dShifter
@@ -93,7 +95,7 @@ def task_core_gauss_readout(
 
     set_random_seed(seed)
 
-    core = TaskDrivenCore2(
+    core = TaskDrivenCore3(
         input_channels=core_input_channels,
         model_name=model_name,
         layer_name=layer_name,
@@ -435,6 +437,9 @@ def custom_task_core_multihead_attention(
     gamma_shifter=0,
     shifter_bias=True,
     elu_offset=-1,
+    stack_pos_encoding=False,
+    n_pos_channels=0,
+    replace_downsampling=False,
 ):
     """
     A Model class of a predefined core (using models from ptrnets). Can be initialized pretrained or random.
@@ -485,6 +490,7 @@ def custom_task_core_multihead_attention(
         final_nonlinearity=final_nonlinearity,
         momentum=momentum,
         fine_tune=fine_tune,
+        replace_downsampling=replace_downsampling,
     )
 
     set_random_seed(seed)
@@ -507,6 +513,8 @@ def custom_task_core_multihead_attention(
         temperature=temperature,  # (learnable-per-neuron, value)
         dropout_pos=dropout_pos,
         layer_norm=layer_norm,
+        stack_pos_encoding=stack_pos_encoding,
+        n_pos_channels=n_pos_channels,
     )
 
     if readout_bias and data_info is None:
@@ -533,7 +541,152 @@ def custom_task_core_multihead_attention(
                 gamma_shifter=gamma_shifter,
             )
 
-    model = EncoderShifter(core, readout, shifter=shifter, elu_offset=elu_offset)
+    model = EncoderShifter(core, readout, shifter=shifter, elu_offset=elu_offset,)
+
+    return model
+
+
+def custom_task_core_shared_multihead_attention(
+        dataloaders,
+        seed,
+        input_channels=1,
+        model_name="vgg19",  # begin of core args
+        layer_name="features.10",
+        pretrained=True,
+        bias=False,
+        final_batchnorm=True,
+        final_nonlinearity=True,
+        momentum=0.1,
+        fine_tune=False,
+        readout_bias=True,
+        gamma_features=3, # start of readout kwargs
+        gamma_query=1,
+        use_pos_enc=True,
+        learned_pos=False,
+        heads=1,
+        scale=False,
+        key_embedding=False,
+        value_embedding=False,
+        temperature=(False,1.0),  # (learnable-per-neuron, value)
+        dropout_pos=0.1,
+        layer_norm=False,
+        data_info=None,
+        shifter=None,
+        shifter_type="MLP",
+        input_channels_shifter=2,
+        hidden_channels_shifter=5,
+        shift_layers=3,
+        gamma_shifter=0,
+        shifter_bias=True,
+        elu_offset=-1,
+        stack_pos_encoding=False,
+        n_pos_channels=0,
+        replace_downsampling=False,
+        gamma_embedding=0,
+        embed_out_dim=None,
+):
+    """
+    A Model class of a predefined core (using models from ptrnets). Can be initialized pretrained or random.
+    Can also be set to be trainable or not, independent of initialization.
+
+    Args:
+        dataloaders: a dictionary of train-dataloaders, one loader per session
+            in the format {'data_key': dataloader object, .. }
+        seed: ..
+        pool_steps:
+        pool_kern:
+        readout_bias:
+        init_range:
+        gamma_readout:
+
+    Returns:
+    """
+
+    if data_info is not None:
+        n_neurons_dict, in_shapes_dict, input_channels = unpack_data_info(data_info)
+    else:
+        if "train" in dataloaders.keys():
+            dataloaders = dataloaders["train"]
+
+        # Obtain the named tuple fields from the first entry of the first dataloader in the dictionary
+        in_name, out_name = next(iter(list(dataloaders.values())[0]))._fields[:2]
+
+        session_shape_dict = get_dims_for_loader_dict(dataloaders)
+        n_neurons_dict = {k: v[out_name][1] for k, v in session_shape_dict.items()}
+        in_shapes_dict = {k: v[in_name] for k, v in session_shape_dict.items()}
+        input_channels = [v[in_name][1] for v in session_shape_dict.values()]
+
+    core_input_channels = (
+        list(input_channels.values())[0]
+        if isinstance(input_channels, dict)
+        else input_channels[0]
+    )
+
+    set_random_seed(seed)
+
+    core = TaskDrivenCore3(
+        input_channels=core_input_channels,
+        model_name=model_name,
+        layer_name=layer_name,
+        pretrained=pretrained,
+        bias=bias,
+        final_batchnorm=final_batchnorm,
+        final_nonlinearity=final_nonlinearity,
+        momentum=momentum,
+        fine_tune=fine_tune,
+        replace_downsampling=replace_downsampling,
+    )
+
+    set_random_seed(seed)
+    core.initialize()
+
+    readout = MultipleSharedMultiHeadAttention2d(
+        core,
+        in_shape_dict=in_shapes_dict,
+        n_neurons_dict=n_neurons_dict,
+        bias=readout_bias,
+        gamma_features=gamma_features,
+        gamma_query=gamma_query,
+        use_pos_enc=use_pos_enc,
+        learned_pos=learned_pos,
+        heads=heads,
+        scale=scale,
+        key_embedding=key_embedding,
+        value_embedding=value_embedding,
+        temperature=temperature,  # (learnable-per-neuron, value)
+        dropout_pos=dropout_pos,
+        layer_norm=layer_norm,
+        stack_pos_encoding=stack_pos_encoding,
+        n_pos_channels=n_pos_channels,
+        embed_out_dim=embed_out_dim,
+        gamma_embedding=gamma_embedding,
+    )
+
+    if readout_bias and data_info is None:
+        for key, value in dataloaders.items():
+            _, targets = next(iter(value))[:2]
+            readout[key].bias.data = targets.mean(0)
+
+    if shifter is True:
+        data_keys = [i for i in dataloaders.keys()]
+        if shifter_type == "MLP":
+            shifter = MLPShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                hidden_channels_shifter=hidden_channels_shifter,
+                shift_layers=shift_layers,
+                gamma_shifter=gamma_shifter,
+            )
+
+        elif shifter_type == "StaticAffine":
+            shifter = StaticAffine2dShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                bias=shifter_bias,
+                gamma_shifter=gamma_shifter,
+            )
+
+    model = EncoderShifter(core, readout, shifter=shifter, elu_offset=elu_offset,)
 
     return model
 
