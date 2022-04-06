5a517b3a6cbf12a2b409623e5816151624ddfef4
KonstantinWilleke <konstantin.willeke@gmail.com>
2022-04-06 16:01:44 -0500

    Add expanded CL stimuli tables.

--- nnvision/tables/experiment.py
+++ nnvision/tables/experiment.py
@@ -125,3 +125,111 @@ class ShiftCLMaskedMEI(dj.Manual):
     shift_mask_key       : longblob
     image                : longblob
     """
+
+
+from nnvision.utility.experiment_helpers.image_processing import shift_image_based_on_masks
+from scipy.ndimage import center_of_mass
+
+@schema
+class GaussianCLMaskedControlExpanded(dj.Computed):
+    definition = """
+    -> GaussianCLMaskedControl
+    ---
+    method_fn         : varchar(64)
+    method_hash       : varchar(64)
+    dataset_fn        : varchar(64)
+    dataset_hash      : varchar(64)
+    ensemble_hash     : varchar(64)
+    data_key          : varchar(64)
+    unit_id           : int
+    unit_type         : int
+    mei_seed          : int
+    com_h             : float
+    com_w             : float
+    """
+
+    def make(self, key):
+        primary_keys, secondary_keys, mask = (GaussianCLMaskedControl & key).fetch1("KEY", "image_mask_key", "image_mask")
+        com_h, com_w = center_of_mass(mask)
+        insert_key = {**primary_keys, **secondary_keys, 'com_h': com_h, 'com_w': com_w}
+        self.insert1(insert_key)
+
+
+@schema
+class ShiftCLMaskedControlExpanded(dj.Computed):
+    definition = """
+    -> ShiftCLMaskedControl
+    ---
+    method_fn         : varchar(64)
+    method_hash       : varchar(64)
+    dataset_fn        : varchar(64)
+    dataset_hash      : varchar(64)
+    ensemble_hash     : varchar(64)
+    data_key          : varchar(64)
+    unit_id           : int
+    unit_type         : int
+    image_mei_seed    : int
+    shift_mei_seed    : int
+    com_h             : float
+    com_w             : float
+    shifted_mask      : longblob
+    """
+
+    def make(self, key):
+        keys = (ShiftCLMaskedControl & key).fetch1()
+
+        com_h, com_w = center_of_mass(keys["shift_mask"])
+        shifted_mask = shift_image_based_on_masks(keys["image_mask"], keys["image_mask"], [keys["shift_mask"]])[0]
+        image_mei_seed = keys["image_mask_key"]["mei_seed"]
+        shift_mei_seed = keys["shift_mask_key"]["mei_seed"]
+
+
+        insert_key = {**keys,
+                      **keys["image_mask_key"],
+                      'com_h': com_h,
+                      'com_w': com_w,
+                      'image_mei_seed': image_mei_seed,
+                      'shift_mei_seed': shift_mei_seed,
+                      'shifted_mask': shifted_mask}
+        self.insert1(insert_key, ignore_extra_fields=True)
+
+
+@schema
+class GaussianCLMaskedMEIExpanded(dj.Computed):
+    definition = """
+    -> GaussianCLMaskedMEI
+    ---
+    com_h           : longblob
+    com_w                : longblob
+    """
+
+    def make(self, key):
+        primary_keys, mask = (GaussianCLMaskedMEI & key).fetch1("KEY", "image_mask")
+        com_h, com_w = center_of_mass(mask)
+        insert_key = {**primary_keys, 'com_h': com_h, 'com_w': com_w}
+        self.insert1(insert_key)
+
+
+@schema
+class ShiftCLMaskedMEIExpanded(dj.Computed):
+    definition = """
+    -> ShiftCLMaskedMEI
+    ---
+    shift_mei_seed    : int
+    com_h             : float
+    com_w             : float
+    shifted_mask      : longblob
+    """
+
+    def make(self, key):
+        primary_keys, shift_key, shift_mask, image_mask = (ShiftCLMaskedMEI & key).fetch1("KEY", "shift_mask_key", "shift_mask", "image_mask")
+        com_h, com_w = center_of_mass(shift_mask)
+
+        shifted_mask = shift_image_based_on_masks(image_mask, image_mask, [shift_mask])[0]
+        shift_mei_seed = shift_key["mei_seed"]
+        insert_key = {**primary_keys,
+                      'com_h': com_h,
+                      'com_w': com_w,
+                      'shifted_mask': shifted_mask,
+                      'shift_mei_seed':shift_mei_seed }
+        self.insert1(insert_key)
