4f9f561d1cbfc46d66aad3c026fb5022127248f3
KonstantinWilleke <konstantin.willeke@gmail.com>
2023-01-09 11:28:38 +0100

    remove include_package_data=True from setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "neuralpredictors @ git+https://github.com/KonstantinWilleke/neuralpredictors.git@transformer_readout",
         "mei @ git+https://github.com/sinzlab/mei.git@inception_loop",
     ],
-    include_package_data=True,
     package_data={
         "nnvison": [
             "data/model_weights/*.pth.tar",
