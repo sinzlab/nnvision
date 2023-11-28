61c8a731c00deefffa27271072501d929f0c472f
KonstantinWilleke <konstantin.willeke@gmail.com>
2023-11-28 09:36:13 -0600

    run script convnext data driven.

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ dataset_keys = [dict(dataset_hash="9ef1991a6c99e7d5af6e2a51c3a537a6"),
                 dict(dataset_hash="7214ff189a2fb06af589d9a96e2cf140"),
                ]
 trainer_keys = dict(trainer_hash="e145fd0f8537a754b8972d894ed2820d")
-model_keys = (Model & "model_ts > '2023-11-28 05'").fetch("KEY")
+model_keys = (Model & "model_ts > '2023-11-28 14'").fetch("KEY")
 pop_key = dj.AndList([dataset_keys, trainer_keys, model_keys, dict(seed=1000)])
 
 TrainedModel().populate(pop_key,
