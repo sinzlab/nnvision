e810d16596a2003c37f87522b927a8df663de346
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2025-02-08 12:21:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='nnvision',
-    version='0.1',
+    version='0.1.2',
     description='Envisioning the biological visual system with DNN',
     author='Konstantin Willeke',
     author_email='konstantin.willeke@gmail.com',
